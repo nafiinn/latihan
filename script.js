af2abb8c6d3db85d07ff607a627e9e553fa3efd3
nafiinn <nafinabqori29@gmail.com>
2024-10-15 10:45:32 +0700

    add index.html

---
+++ script.js
@@ -0,0 +1,94 @@
+// let x = 10;
+// let a = prompt('masukan angka');
+// let y = prompt('masukan angka ke-2')
+// document.write(x+a)
+// console.log(x+y)
+
+// var nomor = prompt("1+1 sama dengan")
+// if (nomor == 2) {
+//     alert("betul")
+// } else {
+//     alert("salah nigga")
+// }
+
+// var nama = prompt("masukan")
+// alert(`selamat datang ${nama}`);
+
+
+// let name = prompt("Masukan Nama"), 
+// jurusan = prompt("Masukan Jurusan"), 
+// alamat = prompt("Masukan Alamat")
+// alert(`selamat datang ${name}, di jurusan ${jurusan}, aku tebak pasti rumahnhya di ${alamat}`);
+
+// var kalimat = 1
+// while (kalimat > 0) {
+//     console.log("hitung mundur: "+ kalimat); 
+//     kalimat++;
+// }
+// document.write("beres")
+
+// var n = 10
+// while (n <= 5){
+//     console.log(n);
+//     n++
+// }
+
+// for (let i = 2; i <= 10; i++) {
+//     if (i % 2 !== 0) {
+//         console.log(i)
+//     }
+// }
+
+// let text = '';
+// for (let i = 1; i <= 3; i++){
+//     text = `${i*2} `;
+//     document.write(text)
+// }
+    
+// let nama = "alif"
+// let usia = 18;
+// let pesan = `nama saya ${nama}, usia saya ${usia} tahun.`
+// console.log(pesan)
+
+//menghitung luas persegi panjang
+// let panjang = prompt(`masukan panjangnya`)
+// let lebar = prompt(`masukan lebar`)
+//     hasil = panjang*lebar
+
+//     alert(`luas persegi panjang adalah ${hasil}`)
+
+// //menghitung luas lingkaran
+// let jari2 = prompt(`masukan jari2 lingkaran mu`)
+// hasil2 = 3.14*jari2*jari2
+
+//     alert(`luas lingkaran anda adalah ${hasil2}`)
+
+// for (let i = 1; i<=5; i++) {
+
+//         console.log(i)
+// }
+
+// function cekAngka() {
+//     let angka = prompt("Masukkan sebuah angka:");
+
+//     // // Ubah input menjadi angka
+//     // angka = parseInt(angka);
+
+    // Cek apakah angka genap atau ganjil
+    // if (angka % 2 == 0) {
+    //     alert("genap");
+    // } else {
+    //     alert("ganjil cok");
+    // }
+
+
+// // Panggil fungsi    
+// cekAngka();
+
+// var ngulang = true
+// while(ngulang){
+//     console.log (`tes`)
+//     ngulang = confirm (`lagi nih`)
+// }
+
+
